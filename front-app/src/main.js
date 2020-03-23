9d3490593b4eec385b2c820ad2aa8a4527f0c72d
Oussama ELLEUCH <oelleuch@omnilog.fr>
2020-03-23 17:16:45 +0100

    initial 2.0

--- front-app/src/main.js
+++ front-app/src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import Vue from 'vue'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
+import VueSidebarMenu from 'vue-sidebar-menu'
+import 'vue-sidebar-menu/dist/vue-sidebar-menu.css'
 
 Vue.use(VueMaterial)
+Vue.use(VueSidebarMenu)
 Vue.config.productionTip = false
 
 new Vue({
